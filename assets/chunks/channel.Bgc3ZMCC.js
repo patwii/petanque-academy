7c38dc07adb9fe60920094f871959ec3ba957a24
patrik-wiik-visma <patrik.wiik@visma.com>
2026-01-04 20:02:13 +0100

    Deploy: Remove Google Ads and add Better vs More shots section

--- assets/chunks/channel.Bgc3ZMCC.js
+++
@@ -1,1 +0,0 @@
-import{U as a,C as s}from"../app.Cn1sbNOS.js";const o=(o,p)=>a.lang.round(s.parse(o)[p]);export{o as c};
---
+++ assets/chunks/channel.D1I1MDqR.js
@@ -0,0 +1,1 @@
+import{U as a,C as s}from"../app.CZXuKqCh.js";const o=(o,p)=>a.lang.round(s.parse(o)[p]);export{o as c};
